2a47f9ef85c704a3482714b4baafa1edef5134ef
Strat00s <l.bastyr@seznam.cz>
2023-01-07 00:34:08 +0100

    some start

---
+++ main_proj/DE2_Default/pwm.v
@@ -0,0 +1,18 @@
+/*  Simple 5Khz PWM module
+    Start HIGH, stay HIGH until `cnt` = desired_input, reset at after 5000 cycles
+*/
+
+
+module PWM (input clk, input reg[8:0] duty_cycle);
+    output out;
+    
+    initial begin
+    
+    end
+
+    always @(posedge CLOCK_50) begin
+
+
+    end
+
+endmodule
