5561299ad29d9e8dda8772845f908917f11beb6e
Strat00s <l.bastyr@seznam.cz>
2023-01-07 13:34:14 +0100

    working PWM

--- main_proj/DE2_Default/DE2_Default.v
+++ main_proj/DE2_Default/DE2_Default.v
@@ -1,50 +1,65 @@
+//using all RED LEDS (LEDR)
 
 module DE2_Default (input CLOCK_50, output [17:0] LEDR);
     //constants
-    parameter MAIN_FREQ = 50000000; //50MHz
-    parameter PWM_FREQ = 5000;      //5KHz
+    parameter MAIN_FREQ   = 50000000;   //CLOCK_50 frequency
+    parameter PWM_FREQ    =     1000;   //base PWM frequency
+    parameter PWM_S_CNT   =      200;   //sample count ("precision")
 
-    //pwm clock vars
-    reg [31:0] cnt;
-    reg pwm_clk;
+    //pwm vars
+    reg [31:0] pwm_d_cnt;   //pwm division counter
+    reg pwm_clk;            //pwm clock output
 
+    //LED duty cycle array
+    //reg [7:0] leds[17:0];
 
     //reg [31:0] t_cnt;
     wire state;
 
+    reg [31:0] dim_cnt;
+
+
     //setup
     initial begin
-        cnt <= 0;
-        pwm_clk <= 0;
+        pwm_d_cnt <= 0;
+        pwm_clk   <= 0;
+        on_time   <= 10;
 
-        //t_cnt <= 0;
-        //state <= 0;
+        dim_cnt <= 0;
     end
-
 
     //"main"
     always @(posedge CLOCK_50) begin
-        if (cnt == MAIN_FREQ / (PWM_FREQ * 2)) begin //*2 becuase we are flipping the state
+        //generate PWM clock
+        //desired frequency * samples * 2; *2 as we are flipping the state
+        if (pwm_d_cnt == MAIN_FREQ / (PWM_FREQ * PWM_S_CNT * 2)) begin
+            pwm_d_cnt <= 0;
             pwm_clk <= ~pwm_clk;
-            cnt <= 0;
         end
         else
-            cnt <= cnt + 1;
+            pwm_d_cnt <= pwm_d_cnt + 1;
+        
+        //gradually change PWM
+        if (dim_cnt == MAIN_FREQ / 100) begin
+            dim_cnt <= 0;
+            
+        end
+        else
+            dim_cnt = dim_cnt + 1;
+    
+
     end
 
-    //always @(posedge pwm_clk) begin
-    //    if (t_cnt == PWM_FREQ) begin
-    //        state <= ~state;
-    //        t_cnt <= 0;
-    //    end
-    //    else
-    //        t_cnt <= t_cnt + 1;
-    //end
+
 
     PWM pwm (
         .clk(pwm_clk),
+        .on_time(on_time),
         .out(state)
     );
+
+
+    //assign pwm_on_time = on_time;
 
     assign	LEDR[0]  = state;
     assign	LEDR[2]  = state;
--- main_proj/DE2_Default/PWM.v
+++ main_proj/DE2_Default/PWM.v
@@ -1,49 +1,25 @@
 
-//module PWM (input clk, input [31:0] on_time, output out);
-//    parameter PWM_FREQ = 5000;
-//
-//    reg [31:0] cnt;
-//
-//    initial begin
-//        cnt <= 0;
-//    end
-//
-//    always @(posedge clk) begin
-//        if (cnt == PWM_FREQ)
-//            cnt <= 0;
-//        else
-//            cnt <= cnt + 1;
-//    end
-//
-//    assign out = cnt < on_time ? 1:0; 
-//
-//endmodule
-
-
-module PWM (input clk, output out);
+module PWM (input clk, input [7:0] on_time, output out);
     //constants
-    parameter PWM_FREQ = 5000;      //5KHz
+    parameter SAMPLES = 200;
 
-    reg [31:0] cnt;
-    reg state;
+
+    reg [7:0] cnt;
 
     //setup
     initial begin
         cnt <= 0;
-        state <= 0;
     end
 
 
     //"main"
     always @(posedge clk) begin
-        if (cnt == PWM_FREQ) begin
-            state <= ~state;
+        if (cnt == SAMPLES)
             cnt <= 0;
-        end
         else
             cnt <= cnt + 1;
     end
 
-    assign out = state;
+    assign out = (cnt <= on_time) ? 1:0;
 endmodule
 
